1550c4dc0b77d9a39e82d074b1ff7563b0c8549a
SuriyaPrakash-05 <sur05012001.com>
2025-06-30 20:06:14 +0530

    tasks

---
+++ JsTasks.js
@@ -0,0 +1,223 @@
+//Find the max or min of three numbers
+// let a=100000
+// let b=100
+// let c=1000
+// if(a>b && a>c){
+//     console.log("Max:",a)
+// }
+// else if(b>a && b>c){
+//     console.log("Max:",b)
+// }
+// else{
+//     console.log("Max:",c)
+// }
+// if(a<b && a<c){
+//     console.log("Min:",a)
+// }
+// else if(b<a && b<c){
+//     console.log("Min:",b)
+// }
+// else{
+//     console.log("Min:",c)
+// }
+
+
+
+//Find the grade of the given marks
+// let a=10
+// let b=10
+// let c=20
+// let d=12
+// let e=1
+
+// let total=500
+// let sum=a+b+c+d+e
+
+// let percentage=(sum/total)*100
+// console.log("Percentage:",percentage)
+
+// if(percentage>=90){
+//     console.log("Grade A")
+// }
+// else if(percentage>=80 && percentage<90){
+//     console.log("Grade B")
+// }
+// else if(percentage>=70 && percentge<80){
+//     console.log("grade C")
+// }
+// else if(percentage>=60 && percentage<70){
+//     console.log("Grade D")
+// }
+// else if(percentage>=50 && percentage<60){
+//     console.log("Grade E")
+// }
+// else{
+//     console.log("Grade F")
+// }
+
+
+
+
+//FizzBuzz
+// let n = 15
+// for (let i = 1; i <= n; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log("Fizz Buzz")
+//     }
+//     else if (i % 3 === 0) {
+//         console.log("Fizz")
+//     }
+//     else if (i % 5 === 0) {
+//         console.log("Buzz")
+//     }
+//     else {
+//         console.log(i)
+//     }
+// }
+
+
+
+//Eligible to vote or not
+// let age=100
+// if(age>=18){
+//     console.log('Eligible to vote')
+// }
+// else{
+//     console.log('Not eligible')
+// }
+
+
+//Eligible to ride rollercoaster accept age above 165cm
+// let a=180
+// if(a>165){
+//     console.log('Eligible to ride')
+// }
+// else{
+//     console.log('Not eligible to ride')
+// }
+
+
+
+// Print no from 1 to 10
+// let num=1
+// while(num<=10){
+//     console.log(num)
+//     num++
+// }
+
+
+
+//Print even numbers between 1 and 20.
+// let num=1
+// while(num<=20){
+//     if(num%2===0){
+//         console.log(num)   
+//     }
+//     num++
+// }
+
+
+
+
+//Count how many numbers from 1 to 100 are divisible by 7.
+// let count=0
+// for(i=1;i<=100;i++){
+//     if(i%7===0){
+//         count++
+//     }
+// }
+// console.log(count)
+
+
+
+//Print all odd numbers between 1 and 50.
+// for(let i=1;i<=50;i++){
+//     if(i%2!==0){
+//         console.log(i)
+//     }
+// }
+
+
+
+//Print the multiplication table of a number 
+// let num=8
+// for(let i=1;i<=10;i++){
+//     console.log(num+"x"+i+"="+num*i)
+// }
+
+
+//Print the factorial of a given number using a loop.
+// let num=8
+// let factorial=1
+// for(let i=1;i<=num;i++){
+//    fact=factorial*=i   
+// }
+// console.log(fact)
+
+
+
+//Print numbers from 100 to 1 in reverse order.
+// for(let i=100;i>=1;i--){
+//     console.log(i)
+// }
+
+
+
+//Print the square of each number from 1 to 20.
+// for (let i = 1; i <= 20; i++) {
+//     console.log("Square of " + i + " is " + i * i)
+// }
+
+
+//Print only the prime numbers from 1 to 50.
+// for(let i=2;i<=50;i++){
+//     let isPrime=true
+//     for(let j=2;j<i;j++){
+//         if(i%j===0){
+//             isPrime=false
+//             break
+//         }
+//     }
+//     if(isPrime){
+//         console.log(i)
+//     }
+// }
+
+
+//Count how many even and odd numbers are between 1 and 100.
+// let even=0
+// let odd=0
+// for(i=1;i<=100;i++){
+//     if(i%2===0){
+//         even++
+//     }
+//     else{
+//         odd++
+//     }
+// }
+// console.log("Even count:",even)
+// console.log("Odd count:",odd)
+
+
+
+//Print all numbers from 1 to 100, but stop when the number is 45.
+// for (let i = 1; i <= 100; i++) {
+//     if (i == 46) {
+//         break
+//     }
+//     console.log(i)
+// }
+  
+
+
+
+
+//Print all numbers from 1 to 50, but skip numbers divisible by 4.
+// for(let i=1;i<=50;i++){
+//     if(i%4!==0){
+//         console.log(i)
+//     }
+// }
+
+
+
